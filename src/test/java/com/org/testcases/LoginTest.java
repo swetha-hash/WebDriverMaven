f948601cf7273e113729b9e374c3d47e8ac41d79
unknown <muchapathi.swetha@gmail.com>
2020-05-24 12:31:37 +0530

    My first jenkins commit

--- src/test/java/com/org/testcases/LoginTest.java
+++ src/test/java/com/org/testcases/LoginTest.java
@@ -28,6 +28,9 @@ public class LoginTest {
 				"/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div/span/span"))
 				.click();
 		System.out.println("Just login");
+		/*
+		 * Added new comments
+		 */
 		
 	}
 
